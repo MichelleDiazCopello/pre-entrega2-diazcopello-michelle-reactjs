749ba3d96c79eb68e16e9d76c3043f4842f1a815
Michelle Diaz Copello <m.diazcopello@gmail.com>
2023-11-29 22:31:13 -0300

    cart

--- src/components/cart-widget/cart-widget.jsx
+++ src/components/cart-widget/cart-widget.jsx
@@ -1,13 +1,18 @@
 import './cart-widget.css';
 import cart from './assets/cart-shopping-solid.svg';
+import { useContext } from 'react';
+import { CartContext } from '../../context/cartContext';
+import { Link } from 'react-router-dom';
 
 const CartWidget = () => {
+    const { totalQuantity } = useContext ( CartContext ); 
+    
     return (
-        <div className= 'Cart-Widget'>
-            <img src= {cart} alt="cart-widget" width="15px" />
-            <p className= 'NumberExample'>3</p>
-        </div>
-    )
-}
+        <Link to='/cart' className='Cart-Widget' style={{ display: totalQuantity > 0 ? 'block' : 'none' }}>
+            <img className='CartImg' src={cart} alt="Cart Widget" />
+            { totalQuantity }
+        </Link>
+    );
+};
 
-export default CartWidget
+export default CartWidget;
---
+++ src/components/cart/cart.jsx
@@ -0,0 +1,32 @@
+import './cart.css';
+import { useContext } from 'react';
+import { CartContext } from '../../context/cartContext';
+import CartItem from '../cartItem/cartItem';
+import { Link } from 'react-router-dom';
+
+const Cart = () => {
+    const { cart, clearCart, totalQuantity, total } = useContext ( CartContext );
+
+    if ( totalQuantity === 0 ) {
+        return (
+            <div>
+                <h1>No existen productos en el carrito</h1>
+                <Link to='/' className='Option'>Productos</Link>
+            </div>
+        );
+    };
+
+    return (
+        <div>
+            { cart.map ( p => <CartItem key= { p.id } { ...p }/>)};
+            <h3>Total: $ {total}</h3>;
+            <button onClick={ () => clearCart () } className='Button'>Vaciar carrito</button>;
+            <Link to='/checkout' className='Option'></Link>
+        </div>
+    );
+};
+
+export default Cart;
+
+// ver / crear el archivo cartItem.
+// agregar cart a App
